c49210bc9afe906f161e33f8e46a40598fd07d9f
aishatr <aishatmidorbusiness@gmail.com>
2019-12-09 16:39:56 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/BullHornFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullHornFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullHornFinalApplication.class, args);
+	}
+
+}
